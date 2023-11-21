6ffccd7ee4122fc28b37c4929c434cba64f5eec4
SalviShahzad <35057846+SalviShahzad@users.noreply.github.com>
2023-11-21 13:36:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,30 @@ console.log(val)
 }).catch((val)=> {
 console.log(val)
 })
+
+// Promise.all implementation
+const promise1 = new Promise((resolve,reject)=> {
+setTimeout(()=>{
+resolve("Yes!")
+}, 1000)
+reject("No! 1")
+})
+
+const promise2 = new Promise((resolve,reject)=> {
+setTimeout(()=>{
+resolve("Yes! 2")
+}, 1000)
+})
+
+const promise3 = new Promise((resolve,reject)=> {
+setTimeout(()=>{
+resolve("Yes! 3")
+}, 1000)
+reject("No!")
+})
+
+Promise.all([promise1, promise2, promise3]).then((values)=> {
+console.log(values)
+}).catch((err)=> {
+console.log(err) // Consoles No! 1
+})
